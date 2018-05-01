c7092b9fb551073f4754219aaaba12abba1bc9e5
Nishant <nishantsalhotra.ns@gmail.com>
2018-05-02 02:19:14 +0530

    Exercises: Add exerciese to segment-1

---
+++ __test__/lowercaseKeys.test.js
@@ -0,0 +1,8 @@
+const lowercaseKeys = require('../src/lowercaseKeys');
+
+describe('lowercaseKeys', () => {
+  test('should return', () => {
+    expect(lowercaseKeys({ FOO: true }).foo).toBe(true);
+    expect(lowercaseKeys({ FOO: true, bAr: true }).bar).toBe(true);
+  });
+});
---
+++ src/roundTo.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+
+/*
+roundTo(1.234, 2);
+//=> 1.23
+
+roundTo.up(1.234, 2);
+//=> 1.24
+
+roundTo.down(1.234, 2);
+//=> 1.23
+
+Look at the tests for 'roundTo' for all cases
+*/
+
+function round(method, input, precision) {
+
+}
+
+module.exports = round.bind(null, 'round');
+module.exports.up = round.bind(null, 'ceil');
+module.exports.down = round.bind(null, 'floor');
