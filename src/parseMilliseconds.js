667c62b6e745b9425f59a39ccb22afd1488ecfb3
Sharmishtha <sharmistha.gemini@gmail.com>
2018-06-09 00:03:19 +0530

    Initial commit

--- src/parseMilliseconds.js
+++ src/parseMilliseconds.js
@@ -1,8 +1,9 @@
 // parseMs(1337000001);
 // => { days: 15, hours: 11, minutes: 23, seconds: 20, milliseconds: 1 }
 
-function parseMs() {
+// function parseMs() {
+//     var date1 = new Date()
 
-}
+// }
 
-module.exports = parseMs;
+// module.exports = parseMs;
