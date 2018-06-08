b36ffb61e15eab6ef0d10f54f1de038544353337
Nikhil Arora <naror200@gmail.com>
2018-06-09 03:40:09 +0530

    WIP: partial commits

--- src/daysInMonth.js
+++ src/daysInMonth.js
@@ -1,9 +1,10 @@
 // Get the number of days in a given month
-
+/* eslint-disable */
 // countDaysInMonth(2016, 11); //=> 31
 // countDaysInMonth(2016, 8); //=> 30
 
 function countDaysInMonth(year, month) {
+  month += 1;
   return new Date(year, month, 0).getDate();
 }
 
