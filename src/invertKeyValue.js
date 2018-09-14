a1b27e5f43bc111fdf61339b7621f25ce742277e
Bharani <bharanidharan.nb@gmail.com>
2018-09-15 00:33:35 +0530

    invertKeyValue

--- src/invertKeyValue.js
+++ src/invertKeyValue.js
@@ -1,8 +1,12 @@
 // invertKv({foo: 'bar', unicorn: 'rainbow'});
 // => {bar: 'foo', rainbow: 'unicorn'}
 
-function invertKeyValue() {
-
+function invertKeyValue(object) {
+  const newObj = {};
+  Object.keys(object).forEach((key) => {
+    newObj[object[key]] = key;
+  });
+  return newObj;
 }
 
 module.exports = invertKeyValue;
