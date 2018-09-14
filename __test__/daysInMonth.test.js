0cb7975aa96da136a15b99eca3639e97f58d6400
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-14 23:54:55 +0530

    Fixed daysInMonth

--- __test__/daysInMonth.test.js
+++ __test__/daysInMonth.test.js
@@ -2,7 +2,7 @@ const { countDaysInMonth } = require('../src/daysInMonth');
 
 describe('countDaysInMonth', () => {
   test('should return correct number of days', () => {
-    expect(countDaysInMonth(2014, 4)).toBe(30);
+    expect(countDaysInMonth(2014, 4)).toBe(31);
     expect(countDaysInMonth(2016, 1)).toBe(29);
   });
 });
--- src/daysInMonth.js
+++ src/daysInMonth.js
@@ -3,8 +3,17 @@
 // countDaysInMonth(2016, 11); //=> 31
 // countDaysInMonth(2016, 8); //=> 30
 
+const daysInMonthNonLeap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const daysInMonthLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function isLeapYear(year) {
+  return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
+}
+
 function countDaysInMonth(year, month) {
-  return year + month;
+  if (isLeapYear(year)) return daysInMonthLeap[month];
+
+  return daysInMonthNonLeap[month];
 }
 
 module.exports = { countDaysInMonth };
