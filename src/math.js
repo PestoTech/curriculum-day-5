aa78ed0896708672c02ddbd5bd745032bc879fdb
Bharani <bharanidharan.nb@gmail.com>
2018-09-15 00:04:37 +0530

    Use Math functions

--- src/math.js
+++ src/math.js
@@ -1,13 +1,14 @@
 // For reference read this https://www.w3schools.com/js/js_math.asp
 
 // Calculate square root of 49 using Math Object
-const sqrt = 5;
+const sqrt = Math.sqrt(49);
 
 // Calculate 4 to the power 2 using Math
-const power = 5;
+// eslint-disable-next-line
+const power = Math.pow(4, 2);
 
 // Round up 6.7 to its nearest integer
-const round = 5;
+const round = Math.round(6.7);
 
 module.exports = {
   sqrt,
