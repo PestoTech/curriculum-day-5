80c8c97ba1bab57205b4af1400d8e2e32ea781e4
shashank36 <s.shashank36@gmail.com>
2018-09-15 01:34:48 +0530

    [WIP] Segment-1 commit-3

--- src/lowercaseKeys.js
+++ src/lowercaseKeys.js
@@ -1,8 +1,15 @@
 // lowercaseKeys({FOO: true, bAr: false});
 // => { foo: true, bar: false }
 
-function lowercaseKeys() {
-
+function lowercaseKeys(obj) {
+  let obj2 = {};
+  let key2;
+  obj2 = Object.keys(obj).reduce((acc, key) => {
+    key2 = key.toLocaleLowerCase();
+    acc[key2] = obj[key];
+    return acc;
+  }, {});
+  return obj2;
 }
 
 module.exports = lowercaseKeys;
