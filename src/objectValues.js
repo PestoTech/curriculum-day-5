7b9df7b1019c433320b3efe78db203e61de6f470
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-15 00:22:22 +0530

    Fixed objectValues

--- src/objectValues.js
+++ src/objectValues.js
@@ -4,7 +4,14 @@
 // //=> [0, 1]
 // Do not use Object.values
 function objectValues(object) {
+  const ret = [];
 
+  const keys = Object.keys(object);
+  for (let i = 0; i < keys.length; i += 1) {
+    ret.push(object[keys[i]]);
+  }
+
+  return ret;
 }
 
 module.exports = objectValues;
