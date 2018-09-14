9a907970ecdabab601345b40c3eb9c345f36ce1e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-15 00:09:50 +0530

    Fixed invertKeyValue

--- src/invertKeyValue.js
+++ src/invertKeyValue.js
@@ -1,8 +1,13 @@
 // invertKv({foo: 'bar', unicorn: 'rainbow'});
 // => {bar: 'foo', rainbow: 'unicorn'}
 
-function invertKeyValue() {
+function invertKeyValue(obj) {
+  const ret = {};
 
+  const keys = Object.keys(obj);
+  for (let i = 0; i < keys.length; i += 1) ret[obj[keys[i]]] = keys[i];
+
+  return ret;
 }
 
 module.exports = invertKeyValue;
